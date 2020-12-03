40dfd122cbe2e83b2f5cdbf74602e26f3427a0a9
melha <melhamra98@gmail.com>
2020-12-03 14:34:03 +0100

    create signup component, create from for registration and implement validations

--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -1,15 +1,41 @@
+import { PasswordValidator } from './../../validator/password.validator';
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
-  styleUrls: ['./signup.component.css']
+  styleUrls: ['./signup.component.css'],
 })
 export class SignupComponent implements OnInit {
 
-  constructor() { }
+  signUpForm: FormGroup;
+
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
+    this.signUpForm = this.fb.group({
+      username: ['', [Validators.required, Validators.minLength(4), Validators.maxLength(12)]],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(4), Validators.maxLength(12)]],
+      confirmPassword: ['', [Validators.required]]
+    }, { validators: PasswordValidator });
+  }
+
+  get username() {
+    return this.signUpForm.get('username');
+  }
+
+  get email() {
+    return this.signUpForm.get('email');
+  }
+
+  get password() {
+    return this.signUpForm.get('password');
+  }
+
+  get confirmPassword() {
+    return this.signUpForm.get('confirmPassword');
   }
 
 }
